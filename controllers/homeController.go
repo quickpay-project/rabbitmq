50f5b60d306b297f8223100b038a9d2618046862
Joe Dev <131217254+krieangkrai-tum@users.noreply.github.com>
2025-08-01 14:19:07 +0700

    create exchange name fix1

--- controllers/homeController.go
+++ controllers/homeController.go
@@ -10,18 +10,13 @@ import (
 	rabbitmqconnect "github.com/celalsahinaltinisik/rabbitMQ"
 )
 
-// type request struct {
-// 	Name string
-// 	Age  int
-// 	Car  string
-// }
-
 type Functions struct{}
 
 func (m Functions) Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "home")
 	log.Println(r.Body)
 }
+
 func (m Functions) Consume(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "consume")
 	rabbit := rabbitmqconnect.RabbitMQ{QueueName: "defaultqueuue"}
@@ -29,17 +24,22 @@ func (m Functions) Consume(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m Functions) Publish(w http.ResponseWriter, r *http.Request) {
-
 	body, err := io.ReadAll(r.Body)
 	errors.FailOnError(err, "Failed to readall body request")
 	rabbit := rabbitmqconnect.RabbitMQ{Body: string(body), QueueName: "defaultqueuue"}
 	rabbit.Puplish()
-	// fmt.Fprintln(w, "home")
-	// request := request{}
-	// decoder := json.NewDecoder(r.Body)
-	// err := decoder.Decode(&request)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// log.Println(request)
+}
+
+func (m Functions) Withdraw(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	errors.FailOnError(err, "Failed to readall body request")
+	rabbit := rabbitmqconnect.RabbitWithdrawMQ{Body: string(body), QueueName: "defaultqueuue"}
+	rabbit.Withdraw()
+}
+
+func (m Functions) Deposit(w http.ResponseWriter, r *http.Request) {
+	body, err := io.ReadAll(r.Body)
+	errors.FailOnError(err, "Failed to readall body request")
+	rabbit := rabbitmqconnect.RabbitDepositMQ{Body: string(body), QueueName: "defaultqueuue"}
+	rabbit.Deposit()
 }
