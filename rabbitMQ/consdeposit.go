c5dbdc9652565748793aaf539f3e69da1b506859
Joe Dev <131217254+krieangkrai-tum@users.noreply.github.com>
2025-08-19 04:48:38 +0700

    deposit withdraw fix error 502 cludfare

--- rabbitMQ/consdeposit.go
+++ rabbitMQ/consdeposit.go
@@ -199,7 +199,7 @@ func (r *RabbitDepositMQ) ConsdepositRPC() {
 		log.Fatalf("❌ Queue declare error: %v", err)
 	}
 
-	workerCount := 30 // ✅ จำนวน worker ที่ process พร้อมกัน
+	workerCount := 50 // ✅ จำนวน worker ที่ process พร้อมกัน
 	if err := ch.Qos(workerCount, 0, false); err != nil {
 		log.Fatalf("❌ QoS set error: %v", err)
 	}
